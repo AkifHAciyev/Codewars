7df363e8e8be04809f5fa663803661276629e504
AkifHaciyev <akif6563@mail.ru>
2022-09-05 00:29:04 +0400

    kt7 MakeFunctionThatDoesArithmeticl

---
+++ kt7/MakeFunctionThatDoesArithmeticl.js
@@ -0,0 +1,44 @@
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+function arithmetic(a, b, operator) {
+	if (operator === 'add') {
+		return a + b;
+	} else if (operator === 'subtract') {
+		return a - b;
+	} else if (operator === 'multiply') {
+		return a * b;
+	} else if (operator === 'divide') {
+		return a / b;
+	}
+}
+
+// orr
+// function arithmetic(a, b, operator) {
+// 	switch (operator) {
+// 		case 'add':
+// 			return a + b;
+// 		case 'subtract':
+// 			return a - b;
+// 		case 'multiply':
+// 			return a * b;
+// 		case 'divide':
+// 			return a / b;
+// 	}
+// }
+
+const chai = require('chai');
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe('Basic tests', () => {
+	it('Testing for fixed tests', () => {
+		assert.strictEqual(arithmetic(1, 2, 'add'), 3, "'add' should return the two numbers added together!");
+		assert.strictEqual(arithmetic(8, 2, 'subtract'), 6, "'subtract' should return a minus b!");
+		assert.strictEqual(arithmetic(5, 2, 'multiply'), 10, "'multiply' should return a multiplied by b!");
+		assert.strictEqual(arithmetic(8, 2, 'divide'), 4, "'divide' should return a divided by b!");
+	});
+});
